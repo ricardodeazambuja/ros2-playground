5f32723807621b09f7a5a8d5dd75871556d22e19
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-01-28 10:24:29 -0500

    Allow to select host dir to mount

--- launch_ros2_desktop.sh
+++ launch_ros2_desktop.sh
@@ -6,9 +6,10 @@ cat << EOF
 Usage: ${0##*/} [OPTIONS]
 Launch a new docker ROS2 container.
     -h,  --help            display this help and exit
+    -d,  --host_dir        directory to be mounted inside ~/host (default: $(pwd))
     -r,  --ros_domain_id   domain ID used with ROS2 (default: 0)
     -lo, --dds_local       set the env variable ROS_LOCALHOST_ONLY=1 to avoid cross talking between machines
-    -e,  --env_var         allow to set an env variable (e.g. "MY_ENV1=some value")
+    -e,  --env_var         allow to set env variables (e.g. "MY_ENV1=some value\nMY_ENV2=2")
     -i,  --image           docker image to use (default: ricardodeazambuja/ros2-galactic-desktop:latest)
     -n,  --name            container's name and hostname (default: ros2-<random 10 chars>)
     -l,  --local           sets it to use --network=host
@@ -36,12 +37,15 @@ GDB=
 LTIME=
 EXT_ENV=
 LHO="--env=ROS_LOCALHOST_ONLY=0"
+HOST_DIR=$(pwd)
 while :; do
     case $1 in
         -h|--help) show_help
         exit 0
         ;;
         -r|--ros_domain_id) ROS_DOMAIN_ID=$2
+        ;;
+        -d|--host_dir) HOST_DIR=$(cd $2; pwd)
         ;;
         -lo|--dds_local) LHO="--env=ROS_LOCALHOST_ONLY=1"
         ;;
@@ -83,7 +87,7 @@ fi
 
 echo "Using a ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
 echo 
-echo "This script mounted the current directory ($(pwd))" 
+echo "This script mounted the directory ($HOST_DIR)" 
 echo "inside the container at /home/ros2user/host."
 echo "Therefore, remember that when you decide to use something like \"rm -rf *\" ;)"
 echo 
@@ -91,7 +95,7 @@ echo
 docker run --rm -it $NET $CAP $NGPU $DRI $GDB $LTIME $LHO $EXT_ENV \
              --name $NAME \
              --user $(id -u):$(id -g) \
-             --volume $(pwd):/home/ros2user/host \
+             --volume $HOST_DIR:/home/ros2user/host \
              --group-add video --group-add sudo \
              --device=/dev/video$VIDEO:/dev/video0 \
              --env=DISPLAY=$DISPLAY \
