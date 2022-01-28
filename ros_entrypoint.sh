9e8aef240dd216d2de66561088afae49def06778
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-01-28 17:03:35 -0500

    Faster than testing all files

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -22,7 +22,8 @@ if [ "$SOURCE_HOST" = "1" ]; then
     # Considering the ros2 workspace will be mounted at ~/host, it will check for a local setup.bash
 
     echo "...and to search for Python packages and add them to PYTHONPATH!"
-    echo "for i in \$(find /home/ros2user/host/); do test \$(basename \"\$i\") = \"__init__.py\" && PYTHONPATH=\$PYTHONPATH:\$(cd \$(dirname \"\$i\"); cd ..; pwd); done" >> /home/ros2user/.bashrc
+    # echo "for i in \$(find /home/ros2user/host/); do test \$(basename \"\$i\") = \"__init__.py\" && PYTHONPATH=\$PYTHONPATH:\$(cd \$(dirname \"\$i\"); cd ..; pwd); done" >> /home/ros2user/.bashrc
+    echo "for i in \$(find /home/ros2user/host/ -name \"__init__.py\"); do PYTHONPATH=\$PYTHONPATH:\$(cd \$(dirname \"\$i\"); cd ..; pwd); done" >> /home/ros2user/.bashrc
     echo "export PYTHONPATH" >> /home/ros2user/.bashrc
     # Above we will go through the dir /home/ros2user/host/ and search for python packages to add them to PYTHONPATH    
 fi
