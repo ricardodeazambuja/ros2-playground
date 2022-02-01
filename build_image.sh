97eb8b50aa47e82830c8b16efe78bbcebd982a82
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-02-01 11:04:47 -0500

    minor change

--- build_image.sh
+++ build_image.sh
@@ -4,12 +4,14 @@ if [[ $# -lt 1 ]] ; then
     echo "Oups, you forgot to choose the arguments!"
     echo "First, is the docker image name (e.g. ricardodeazambuja/ros2-galactic-desktop:latest)."
     echo "Second argument is the ROS2 distro name (default: galactic)."
+    echo "Third argument is used to pass extra args like --no-cache."
     exit 1
 fi
 
 ROS_DISTRO=${2:-galactic}
+NOCACHE=${3:-}
 # --no-cache is useful when you are debugging as it will force to build from scratch
-docker build --no-cache --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
+docker build $NOCACHE --build-arg ROS_DISTRO=$ROS_DISTRO --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
   -f ros2-playground.Dockerfile -t $1 .
 
 # https://stackoverflow.com/a/44683248/7658422
